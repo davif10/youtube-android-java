5316f2e4eec4f7cecb6c2e04bab374b82c1926d0
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2021-02-11 21:33:58 -0300

    -Configurando o SearchView -Configurando a Retrofit

--- ApiYoutube/app/src/main/java/com/davisilvaprojetos/apiyoutube/activity/MainActivity.java
+++ ApiYoutube/app/src/main/java/com/davisilvaprojetos/apiyoutube/activity/MainActivity.java
@@ -6,9 +6,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.davisilvaprojetos.apiyoutube.R;
 import com.davisilvaprojetos.apiyoutube.model.Video;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerVideos;
     private List<Video> videos = new ArrayList<>();
     private AdapterVideo adapterVideo;
+    private MaterialSearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Inicializar componentes
         recyclerVideos = findViewById(R.id.recyclerVideos);
+        searchView = findViewById(R.id.searchView);
 
         //Configura Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -36,6 +42,30 @@ public class MainActivity extends AppCompatActivity {
         recyclerVideos.setHasFixedSize(true);
         recyclerVideos.setLayoutManager(new LinearLayoutManager(this));
         recyclerVideos.setAdapter(adapterVideo);
+
+        //Configura métodos para SearchView
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+
+            }
+        });
     }
 
     private void recuperarVideos(){
@@ -45,5 +75,14 @@ public class MainActivity extends AppCompatActivity {
         Video video2 = new Video();
         video2.setTitulo("Video 2 muito interessante!");
         videos.add(video2);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        MenuItem item = menu.findItem(R.id.menu_search);
+        searchView.setMenuItem(item);
+        return true;
     }
 }
---
+++ ApiYoutube/app/src/main/java/com/davisilvaprojetos/apiyoutube/helper/YoutubeConfig.java
@@ -0,0 +1,7 @@
+package com.davisilvaprojetos.apiyoutube.helper;
+
+public class YoutubeConfig {
+    public static String CANAL_ID="";
+    public static String CHAVE_YOUTUBE_API = "AIzaSyCr1wu-2qV6n9scn2tcrpxJAMJPeFcAgKk";
+    public static  String URL_BASE = "";
+}
